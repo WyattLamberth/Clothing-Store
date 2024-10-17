ed27e993071747fefffc46dd80001e0765eb5325
wyatt lamberth <wyattlamberth@outlook.com>
2024-10-17 17:08:28 -0500

    added a space

--- server/routes.js
+++ server/routes.js
@@ -77,4 +77,25 @@ router.post('/login', async (req, res) => {
   }
 });
 
+
+router.post('/category', async(req, res) => {
+  const connection = await pool.getConnection();
+  try{
+      await connection.beginTransaction();
+      const{name, description, sex} = req.body;
+      const CategoryQuery = 'INSERT INTO categories (name, description, sex) VALUES (?, ?, ?)' // insert query for categories table
+      const [CategoryResult] = await connection.query(CategoryQuery, [name, description, sex]);
+      const category_id = CategoryResult.insertId; // get the id of the newly created category
+      await connection.commit();
+      res.status(201).json({ message: 'Category created successfully', category_id: category_id });
+  } catch (error) {
+      await connection.rollback(); // if there is an error, rollback(undo all changes made to the database)
+      console.error('Error creating category:', error);
+      res.status(400).json({ error: 'Error creating category' });
+  } finally{
+      connection.release(); // release the connection back to the pool
+  }
+});
+
+
 module.exports = router;
