26c682f7b81e4fa27de3b3b192e62d8b56072261
wyatt lamberth <wyattlamberth@outlook.com>
2024-10-16 23:44:44 -0500

    added routes for user reg and login, added passport module for authentication

--- server/db/create_db_and_tables.sql
+++ server/db/create_db_and_tables.sql
@@ -68,7 +68,7 @@ CREATE TABLE payment (
 
 -- Address table
 CREATE TABLE address (
-  address_id INT PRIMARY KEY,
+  address_id INT AUTO_INCREMENT PRIMARY KEY,
   line_1 VARCHAR(255) NOT NULL,
   line_2 VARCHAR(255) DEFAULT 'N/A',
   city VARCHAR(100) NOT NULL,
