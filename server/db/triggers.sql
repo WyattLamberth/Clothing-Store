26cb69e1a8d2541885d9bb419449b71b2ab979e5
gabe-hi <gabez14z14@gmail.com>
2024-10-16 22:07:17 -0500

    trigger

--- server/db/triggers.sql
+++ server/db/triggers.sql
@@ -15,3 +15,13 @@ BEGIN
   END IF;
 END;
 
+CREATE TRIGGER reorder_alert_after_stock_decrease
+AFTER UPDATE ON products
+FOR EACH ROW
+BEGIN
+  -- Check if the new stock quantity is below the reorder threshold
+  IF NEW.stock_quantity < NEW.reorder_threshold THEN
+    INSERT INTO reorder_alerts (product_id, alert_date, quantity_to_reorder)
+    VALUES (NEW.product_id, CURDATE(), NEW.reorder_threshold);
+  END IF;
+END;
