383c2a3230f359ef47d8f5d40eb81174a540d20a
gabe-hi <gabez14z14@gmail.com>
2024-10-27 16:53:35 -0500

    fixed order of permissions and roles script

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 6001;
+const port = process.env.PORT ||5001;
 const { passport, authMiddleware } = require('./middleware/passport-auth');
 const publicRoutes = require('./routes/publicRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 const employeeRoutes = require('./routes/employeeRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+
 require('dotenv').config();
 
 // Middleware for all routes
@@ -21,7 +22,7 @@ app.use('/api', publicRoutes);
 // Protected routes (auth required)
 app.use('/api', authMiddleware.authenticate, customerRoutes);
 app.use('/api', authMiddleware.authenticate, employeeRoutes);
-app.use('/api', authMiddleware.authenticate, adminRoutes);
+app.use('/api/admin', authMiddleware.authenticate, adminRoutes); // Prefix admin routes with '/api/admin'
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
