51da8375808d4e00a46ce85e7a5bc06db272e307
GTP-001 <phu27312345@gmail.com>
2024-11-05 15:41:34 -0600

    Update min max default

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -13,7 +13,7 @@ const ShopPage = () => {
 
   // Set Default
   const priceMin = 0;
-  const priceMax = 20000;
+  const priceMax = 300;
   const [categories_gender, setCategoriesGender] = useState(['M', 'F', 'K']);
 
   // Initialize selectedCategories and selectedGender as empty arrays
