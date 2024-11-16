08a594f8231b84afbf93173cc43045a1f74a16d0
gabe-hi <gabez14z14@gmail.com>
2024-11-16 17:48:58 -0600

    got rid of populate permissions queries

--- server/db/populate_scripts/populate_main.sql
+++ server/db/populate_scripts/populate_main.sql
@@ -227,40 +227,6 @@ INSERT INTO roles (role_id, role_name) VALUES
 (2, 'Employee'),
 (3, 'Admin');
 
--- Populate permissions table with Admin permissions
-INSERT INTO permissions (permission_id, permission_name) VALUES
-(3001, 'Manage Users'),
-(3002, 'Manage Products'),
-(3003, 'Manage Orders'),
-(3004, 'Configure System Settings'),
-(3005, 'Manage Promotions and Discounts'),
-(3006, 'Manage Payment and Shipping Settings'),
-(3007, 'Review and Publish Content'),
-(3008, 'Manage Inventory'),
-(3009, 'View Customer Feedback and Reviews'),
-(3010, 'Create/Edit/Delete Page Content');
-
--- Populate permissions table with Employee permissions
-INSERT INTO permissions (permission_id, permission_name) VALUES
-(2001, 'Add/Edit/Delete Products'),
-(2002, 'View and Manage all Orders'),
-(2003, 'Manage Inventory'),
-(2004, 'Set Product Prices and Promotions'),
-(2005, 'Respond to Customer Reviews'),
-(2006, 'Process Refunds and Returns'),
-(2007, 'View Customer Reviews and Feedback');
-
--- Populate permissions table with Customer permissions
-INSERT INTO permissions (permission_id, permission_name) VALUES
-(1001, 'Browse and Search Products'),
-(1002, 'Add Products to Cart'),
-(1003, 'Make Purchase and Place Orders'),
-(1004, 'View and Manage Personal Account'),
-(1005, 'View Order History'),
-(1006, 'Submit Product Reviews and Feedback'),
-(1007, 'Manage Shipping Addresses and Payment Information');
-
-
 -- Admin Role Permissions
 INSERT INTO role_permissions (role_id, permission_id) VALUES
 (3, 3001),
