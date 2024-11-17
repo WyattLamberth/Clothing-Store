72f0b4f58b8b632c70dd1f1e7ac2caff2ffb4d97
GTP-001 <phu27312345@gmail.com>
2024-11-16 23:48:50 -0600

    change categories id in populate script

--- server/db/populate_scripts/new_populate_main.sql
+++ server/db/populate_scripts/new_populate_main.sql
@@ -253,21 +253,21 @@ CREATE PROCEDURE PopulateProducts()
 BEGIN
     -- Basic products with images
     INSERT INTO products (product_name, category_id, description, price, stock_quantity, reorder_threshold, size, color, brand, image_path) VALUES
-    ('Classic T-Shirt', 5, 'Chat GPT T-shirt', 39.99, 30, 5, 'M', 'Black', 'AutoGenerate', 't_shirt_classic.png'),
+    ('Classic T-Shirt', 4, 'Chat GPT T-shirt', 39.99, 30, 5, 'M', 'Black', 'AutoGenerate', 't_shirt_classic.png'),
     ('Slim Fit Jeans', 1, 'Comfy Slim Denim', 49.99, 100, 10, '32', 'Blue', 'DenimX', 'slim_fit_jeans.jpg'),
-    ('Summer Dress', 16, 'Breezy summer dress', 39.99, 30, 5, 'M', 'Pink', 'Fashionista', 'summer_dress.jpg'),
-    ('Casual Sneakers', 13, 'Casual shoes', 69.99, 75, 10, '9', 'White', '1:1Nike', 'new_sneaker.png'),
+    ('Summer Dress', 17, 'Breezy summer dress', 39.99, 30, 5, 'M', 'Pink', 'Fashionista', 'summer_dress.jpg'),
+    ('Casual Sneakers', 14, 'Casual shoes', 69.99, 75, 10, '9', 'White', '1:1Nike', 'new_sneaker.png'),
     ('Blue Jeans', 1, 'Comfortable blue jeans', 49.99, 100, 10, '32', 'Blue', 'DenimX','blue_jeans.png'),
-    ('Red T-shirt', 5, 'Casual red t-shirt', 19.99, 200, 20, 'L', 'Red', 'TeeTime','red_t_shirt.png'),
+    ('Red T-shirt', 4, 'Casual red t-shirt', 19.99, 200, 20, 'L', 'Red', 'TeeTime','red_t_shirt.png'),
     ('Black Belt', 8, 'Classic black belt', 29.99, 150, 10, 'One Size', 'Black', 'LeatherCo','black_belt.png'),
     ('Winter Jacket', 12, 'Warm winter jacket', 89.99, 50, 5, 'XL', 'Grey', 'WinterGear','winter_jacket_kid.png'),
     ('Running Shoes', 13, 'Lightweight running shoes', 69.99, 75, 10, '9', 'White', 'Speedster','run_shoes_2.png'),
-    ('Pleated Skirt', 18, 'Stylish pleated skirt', 34.99, 40, 5, 'S', 'Pink', 'ChicWear','pleart_skirt.png'),
-    ('Baseball Cap', 20, 'Sporty baseball cap', 14.99, 120, 10, 'One Size', 'Navy', 'CapCo','blue_cap.png'),
+    ('Pleated Skirt', 17, 'Stylish pleated skirt', 34.99, 40, 5, 'S', 'Pink', 'ChicWear','pleart_skirt.png'),
+    ('Baseball Cap', 22, 'Sporty baseball cap', 14.99, 120, 10, 'One Size', 'Navy', 'CapCo','blue_cap.png'),
     ('Graphic T-shirt', 5, 'T-shirt with cool graphic print', 24.99, 180, 15, 'M', 'Black', 'ArtTees','t-shirts-graphic.png'),
-    ('Sweater', 26, 'Cozy sweater for cold weather', 59.99, 60, 8, 'L', 'Green', 'CozyKnits','green_sweater.png'),
-    ('Ankle Socks', 29, 'Pack of ankle socks', 9.99, 300, 30, 'One Size', 'White', 'ComfortFit','ankle_socks.png'),
-    ('Cargo Shorts', 32, 'Durable cargo shorts', 44.99, 80, 10, '34', 'Khaki', 'OutdoorPro','cargo_short.png');
+    ('Sweater', 29, 'Cozy sweater for cold weather', 59.99, 60, 8, 'L', 'Green', 'CozyKnits','green_sweater.png'),
+    ('Ankle Socks', 32, 'Pack of ankle socks', 9.99, 300, 30, 'One Size', 'White', 'ComfortFit','ankle_socks.png'),
+    ('Cargo Shorts', 34, 'Durable cargo shorts', 44.99, 80, 10, '34', 'Khaki', 'OutdoorPro','cargo_short.png');
 END //
 
 -- EVERYTHING BEYOND THIS REQUIRES USERS
